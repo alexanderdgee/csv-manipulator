42e7830da700b5222301c53db26a2a16a4950bfc
Alexander Gee <58729022+alexanderdgee@users.noreply.github.com>
2019-12-14 17:24:05 +0000

    Add utility class to modify a CSV file in-place

---
+++ src/main/java/helpers/CSVUtils.java
@@ -0,0 +1,5 @@
+package helpers;
+
+public class CSVUtils {
+    public static void updateRowValues(String csvFileName, String column, String oldValue, String newValue) {}
+}
---
+++ src/test/java/helpers/CSVUtilsTest.java
@@ -0,0 +1,37 @@
+package helpers;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class CSVUtilsTest {
+    private String sample1 = "filename,origin,metadata,hash";
+    private String sample2 = "file1,London,\"a file about London\",e737a6b0734308a08b8586720b3c299548ff77b846e3c9c89db88b63c7ea69b6";
+    private String sample3 = "file2,Surrey,\"a file about The National Archives\",a4bf0d05d8805f8c35b633ee67dc10efd6efe1cb8dfc0ecdba1040b551564967";
+    private String sample4 = "file55,Londom,\"London was initially incorrectly spelled as Londom\",e737a6b0734308a08b8586720b3c299548ff77b846e3c9c89db88b63c7ea69b6";
+    private String sample4Corrected = "file55,London,\"London was initially incorrectly spelled as Londom\",e737a6b0734308a08b8586720b3c299548ff77b846e3c9c89db88b63c7ea69b6";
+    private String sample5 = "file4,Penrith,\"Lake District National Park info\",a4bf0d05d8805f8c35b633ee67dc10efd6efe1cb8dfc0ecdba1040b551564968";
+    private String sampleName = "sample.txt";
+    private String samplePath = FileUtils.relativePath(sampleName);
+
+    @Test
+    public void testUpdateRowValues() throws IOException {
+        createSample();
+        CSVUtils.updateRowValues(sampleName, "origin", "Londom", "London");
+        List<String> processedLines = FileUtils.readLines(samplePath).collect(Collectors.toList());
+        assertEquals(5, processedLines.size());
+        assertEquals(sample1, processedLines.get(0));
+        assertEquals(sample2, processedLines.get(1));
+        assertEquals(sample3, processedLines.get(2));
+        assertEquals(sample4Corrected, processedLines.get(3));
+        assertEquals(sample5, processedLines.get(4));
+    }
+
+    private void createSample() throws IOException {
+        FileUtils.writeLines(samplePath, List.of(sample1, sample2, sample3, sample4, sample5));
+    }
+}
