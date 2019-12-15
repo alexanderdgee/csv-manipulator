ea3193581dec007cd55a91c7fc2d189a0e12b81a
Alexander Gee <58729022+alexanderdgee@users.noreply.github.com>
2019-12-15 14:54:49 +0000

    Implement the CSV modifier
    Uses the file utils to read and write, and the parser to split to make indexing simple. A future optimisation could do in-place updating of a CSV for efficiency.

--- src/main/java/helpers/CSVUtils.java
+++ src/main/java/helpers/CSVUtils.java
@@ -1,5 +1,60 @@
 package helpers;
 
+import csv.CSV;
+import parser.CSVParser;
+import parser.CSVParserOptions;
+import parser.InvalidCSVException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class CSVUtils {
-    public static void updateRowValues(String csvFileName, String column, String oldValue, String newValue) {}
+    public static void updateRowValues(String csvFileName, String column, String oldValue, String newValue)
+            throws IOException, InvalidCSVException {
+        if (csvFileName == null || column == null || oldValue == null || newValue == null) {
+            throw new RuntimeException("String parameters must be non-null to update values successfully.");
+        }
+        if (csvFileName.isEmpty()) {
+            throw new RuntimeException("Must specify a non-empty filename to update values successfully.");
+        }
+        CSVParserOptions options = new CSVParserOptions();
+        CSV csv = readCSV(csvFileName, options);
+        for (int columnIndex : columnsWithTitle(column, csv)) {
+            updateColumn(columnIndex, csv, oldValue, newValue);
+        }
+        writeCSV(csvFileName, csv, options);
+    }
+
+    public static CSV readCSV(String fileName, CSVParserOptions options) throws InvalidCSVException, IOException {
+        // TODO: read data at once and cope with different line separators
+        String csvString = FileUtils.readLines(fileName).collect(Collectors.joining(System.lineSeparator()));
+        CSVParser parser = new CSVParser();
+        return parser.parse(csvString);
+    }
+
+    public static Collection<Integer> columnsWithTitle(String header, CSV csv) {
+        return IntStream.range(0, csv.getHeaders().size())
+                .filter((int i) -> header.equals(csv.getHeaders().get(i))).boxed().collect(Collectors.toSet());
+    }
+
+    public static void updateColumn(int columnIndex, CSV csv, String oldValue, String newValue) {
+        for (List<String> row : csv.getRows()) {
+            if (row.size() > columnIndex && oldValue.equals(row.get(columnIndex))) {
+                row.set(columnIndex, newValue);
+            }
+        }
+    }
+
+    public static void writeCSV(String fileName, CSV csv, CSVParserOptions options) throws IOException {
+        List<String> output = new ArrayList<>();
+        output.add(String.join(String.valueOf(options.separator), csv.getHeaders()));
+        output.addAll(csv.getRows().stream()
+                .map((List<String> row) -> String.join(String.valueOf(options.separator), row))
+                .collect(Collectors.toList()));
+        FileUtils.writeLines(fileName, output);
+    }
 }
--- src/test/java/helpers/CSVUtilsTest.java
+++ src/test/java/helpers/CSVUtilsTest.java
@@ -1,6 +1,7 @@
 package helpers;
 
 import org.junit.Test;
+import parser.InvalidCSVException;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,13 +17,12 @@ public class CSVUtilsTest {
     private String sample4Corrected = "file55,London,\"London was initially incorrectly spelled as Londom\",e737a6b0734308a08b8586720b3c299548ff77b846e3c9c89db88b63c7ea69b6";
     private String sample5 = "file4,Penrith,\"Lake District National Park info\",a4bf0d05d8805f8c35b633ee67dc10efd6efe1cb8dfc0ecdba1040b551564968";
     private String sampleName = "sample.txt";
-    private String samplePath = FileUtils.relativePath(sampleName);
 
     @Test
-    public void testUpdateRowValues() throws IOException {
+    public void testUpdateRowValues() throws IOException, InvalidCSVException {
         createSample();
         CSVUtils.updateRowValues(sampleName, "origin", "Londom", "London");
-        List<String> processedLines = FileUtils.readLines(samplePath).collect(Collectors.toList());
+        List<String> processedLines = FileUtils.readLines(sampleName).collect(Collectors.toList());
         assertEquals(5, processedLines.size());
         assertEquals(sample1, processedLines.get(0));
         assertEquals(sample2, processedLines.get(1));
@@ -32,6 +32,6 @@ public class CSVUtilsTest {
     }
 
     private void createSample() throws IOException {
-        FileUtils.writeLines(samplePath, List.of(sample1, sample2, sample3, sample4, sample5));
+        FileUtils.writeLines(sampleName, List.of(sample1, sample2, sample3, sample4, sample5));
     }
 }
